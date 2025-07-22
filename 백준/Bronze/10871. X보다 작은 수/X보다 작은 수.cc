3932088481e360011ef3808feafc446807e44823
Eunseo Song <162149585+eunseo9311@users.noreply.github.com>
2025-07-22 15:56:15 +0900

    [Bronze V] Title: X보다 작은 수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, x;
+    cin >> n >> x;
+    
+    for(int i = 0; i < n; i++)
+    {
+        int num;
+        cin >> num;
+
+        if(num < x)
+        cout << num << " ";
+    }
+
+    return 0;
+}
